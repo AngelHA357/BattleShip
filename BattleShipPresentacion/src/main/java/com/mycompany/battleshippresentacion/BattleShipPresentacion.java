b4d3f8dfcb986a15c618b5d5c3f2899b6de6653c
AngelHA357 <159971810+AngelHA357@users.noreply.github.com>
2024-11-04 02:35:13 -0700

    Creación de primeras pantallas.
    Falta controlador de pantallas

---
+++ BattleShipPresentacion/src/main/java/com/mycompany/battleshippresentacion/BattleShipPresentacion.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.battleshippresentacion;
+
+import com.mycompany.battleshippresentacion.pantallas.PantallaInicio;
+
+/**
+ *
+ * @author JoseH
+ */
+public class BattleShipPresentacion {
+
+    public static void main(String[] args) {
+        PantallaInicio inicio = new PantallaInicio();
+        inicio.setVisible(true);
+    }
+}
